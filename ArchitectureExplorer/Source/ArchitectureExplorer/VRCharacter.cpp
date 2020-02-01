60c4fd38d356e93e5d34141929b2d3e56eb76cb6
skoutroberson <skout.roberson@gmail.com>
2020-01-31 17:21:23 -0800

    Character actor now moves with the camera so there is no offset between the character and camera.

--- ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
+++ ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
@@ -10,9 +10,11 @@ AVRCharacter::AVRCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(GetRootComponent());
-	
+	Camera->SetupAttachment(VRRoot);
 
 }
 
@@ -27,6 +29,11 @@ void AVRCharacter::BeginPlay()
 void AVRCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FVector NewCameraOffset = Camera->GetComponentLocation() - GetActorLocation();
+	NewCameraOffset.Z = 0;
+	AddActorWorldOffset(NewCameraOffset);
+	VRRoot->AddWorldOffset(-NewCameraOffset);
 
 }
 
