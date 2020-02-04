46498f6066faea057cb14a41e3c7f5bf44a77c46
skoutroberson <skout.roberson@gmail.com>
2020-02-03 18:58:21 -0800

    feat(character): Player can now teleport Currently working on comparing surface normal of surface hit by camera trace so player cant teleport on walls and ceilings.

--- ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
+++ ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
@@ -6,6 +6,9 @@
 #include "Components/StaticMeshComponent.h"
 #include "DrawDebugHelpers.h"
 #include "Engine/World.h"
+#include "TimerManager.h"
+#include "Components/CapsuleComponent.h"
+#include "MotionControllerComponent.h"
 
 // Sets default values
 AVRCharacter::AVRCharacter()
@@ -19,6 +22,14 @@ AVRCharacter::AVRCharacter()
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(VRRoot);
 
+	LeftController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftController"));
+	LeftController->SetupAttachment(VRRoot);
+	LeftController->SetTrackingSource(EControllerHand::Left);
+
+	RightController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightController"));
+	RightController->SetupAttachment(VRRoot);
+	RightController->SetTrackingSource(EControllerHand::Right);
+
 	DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DestinationMarker"));
 	DestinationMarker->SetupAttachment(GetRootComponent());
 
@@ -29,6 +40,9 @@ void AVRCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	DestinationMarker->SetVisibility(false);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%f %f %f"), GetActorUp)
 }
 
 // Called every frame
@@ -51,12 +65,18 @@ void AVRCharacter::UpdateDestinationMarker()
 	FVector Start = Camera->GetComponentLocation();
 	FVector End = Start + Camera->GetForwardVector() * MaxTeleportDistance;
 
-	FHitResult HitResult;
+	
 	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
 
 	if (bHit)
 	{
+
+		DestinationMarker->SetVisibility(true);
 		DestinationMarker->SetWorldLocation(HitResult.Location);
+	}
+	else
+	{
+		DestinationMarker->SetVisibility(false);
 	}
 
 	//DestinationMarker->SetWorldLocation(CameraTraceResult.ImpactPoint);
@@ -70,6 +90,7 @@ void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
 	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
 	PlayerInputComponent->BindAxis(TEXT("TurnRight"), this, &AVRCharacter::TurnRight);
+	PlayerInputComponent->BindAction(TEXT("Teleport"), IE_Released, this, &AVRCharacter::BeginTeleport);
 
 }
 
@@ -86,5 +107,34 @@ void AVRCharacter::MoveRight(float throttle)
 void AVRCharacter::TurnRight(float throttle)
 {
 	AddControllerYawInput(throttle);
+}
+
+void AVRCharacter::BeginTeleport()
+{
+	if (DestinationMarker->IsVisible())
+	{
+		float dot = FVector::DotProduct(GetActorUpVector(), FVector(0, 0, 1));
+		UE_LOG(LogTemp, Warning, TEXT("DOT: %f"), dot);
+		APlayerController* PC = Cast<APlayerController>(GetController());
+		if (PC != nullptr)
+		{
+			PC->PlayerCameraManager->StartCameraFade(0, 1, TeleportFadeTime, FLinearColor::Black);
+		}
+
+		FTimerHandle Handle;
+		GetWorldTimerManager().SetTimer(Handle, this, &AVRCharacter::FinishTeleport, TeleportFadeTime / 2);
+	}
+}
+
+void AVRCharacter::FinishTeleport()
+{
+	FVector TeleportLocation = DestinationMarker->GetComponentLocation();
+	TeleportLocation.Z += GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+	SetActorLocation(TeleportLocation);
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC != nullptr)
+	{
+		PC->PlayerCameraManager->StartCameraFade(1, 0, TeleportFadeTime, FLinearColor::Black);
+	}
 }
 
