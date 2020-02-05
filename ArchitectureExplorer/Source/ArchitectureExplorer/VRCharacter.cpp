294a6e9999dc8e2b869bc6c8736c2997af3a87b0
skoutroberson <skout.roberson@gmail.com>
2020-02-04 23:36:33 -0800

    feat(character): add mc motion interpreter(sprint) Implememented a motion controller motion interpreter for actions like sprinting and dodging. I am currently trying to code a sprint mechanic, which works right now but is wonky.

--- ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
+++ ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
@@ -9,6 +9,7 @@
 #include "TimerManager.h"
 #include "Components/CapsuleComponent.h"
 #include "MotionControllerComponent.h"
+#include "Misc/FrameRate.h"
 
 // Sets default values
 AVRCharacter::AVRCharacter()
@@ -25,10 +26,12 @@ AVRCharacter::AVRCharacter()
 	LeftController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftController"));
 	LeftController->SetupAttachment(VRRoot);
 	LeftController->SetTrackingSource(EControllerHand::Left);
+	LeftController->bDisplayDeviceModel = true;
 
 	RightController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightController"));
 	RightController->SetupAttachment(VRRoot);
 	RightController->SetTrackingSource(EControllerHand::Right);
+	RightController->bDisplayDeviceModel = true;
 
 	DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DestinationMarker"));
 	DestinationMarker->SetupAttachment(GetRootComponent());
@@ -41,6 +44,9 @@ void AVRCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 	DestinationMarker->SetVisibility(false);
+
+	MCLeftPos = LeftController->GetComponentLocation().Z;
+	MCRightPos = RightController->GetComponentLocation().Z;
 
 	//UE_LOG(LogTemp, Warning, TEXT("%f %f %f"), GetActorUp)
 }
@@ -56,23 +62,83 @@ void AVRCharacter::Tick(float DeltaTime)
 	AddActorWorldOffset(NewCameraOffset);
 	VRRoot->AddWorldOffset(-NewCameraOffset);
 
-	UpdateDestinationMarker();
+	if (bTeleportEnabled)
+	{
+		UpdateDestinationMarker();
+	}
+
+	if (bAction1 && bAction2)
+	{
+		InterpretMCMotion();
+
+		if (bSprint)
+		{
+			MoveForward(1.0f);
+		}
+	}
+}
+
+void AVRCharacter::InterpretMCMotion()
+{
+	if (TickCounter % 4 == 0)
+	{
+		// (x >= 0) ? x : -x
+		float LeftZOffset = LeftController->GetComponentLocation().Z - MCLeftPos;
+		float RightZOffset = RightController->GetComponentLocation().Z - MCRightPos;
+
+		if (LeftZOffset < 0)
+			LeftZOffset = -LeftZOffset;
+		if (RightZOffset < 0)
+			RightZOffset = -RightZOffset;
+
+		//UE_LOG(LogTemp, Warning, TEXT("LEFT: %f"), LeftZOffset);
+		//UE_LOG(LogTemp, Warning, TEXT("RIGHT: %f"), RightZOffset);
+		//UE_LOG(LogTemp, Warning, TEXT("TOTAL: %f"), LeftZOffset + RightZOffset);
+		//UE_LOG(LogTemp, Warning, TEXT("-------------------------------------------------------------"));
+
+		if (LeftZOffset > 6 && RightZOffset > 6)
+		{
+			bSprint = true;
+			StopSprintChecks = 0;
+		}
+		else
+		{
+			StopSprintChecks ++;
+			if (StopSprintChecks > StopSprintMax)
+				bSprint = false;
+		}
+
+		MCLeftPos = LeftController->GetComponentLocation().Z;
+		MCRightPos = RightController->GetComponentLocation().Z;
+	}
+
+	if (TickCounter > 10000)
+	{
+		TickCounter = 0;
+	}
+	TickCounter++;
 
 }
 
 void AVRCharacter::UpdateDestinationMarker()
 {
-	FVector Start = Camera->GetComponentLocation();
-	FVector End = Start + Camera->GetForwardVector() * MaxTeleportDistance;
+	FVector Start = RightController->GetComponentLocation();
+	FVector End = Start + RightController->GetForwardVector() * MaxTeleportDistance;
 
 	
 	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
 
 	if (bHit)
 	{
-
-		DestinationMarker->SetVisibility(true);
-		DestinationMarker->SetWorldLocation(HitResult.Location);
+		if (fabsf(1 - FVector::DotProduct(HitResult.Normal, FVector(0, 0, 1))) < 0.4f)
+		{
+			DestinationMarker->SetVisibility(true);
+			DestinationMarker->SetWorldLocation(HitResult.Location);
+		}
+		else
+		{
+			DestinationMarker->SetVisibility(false);
+		}
 	}
 	else
 	{
@@ -91,12 +157,23 @@ void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
 	PlayerInputComponent->BindAxis(TEXT("TurnRight"), this, &AVRCharacter::TurnRight);
 	PlayerInputComponent->BindAction(TEXT("Teleport"), IE_Released, this, &AVRCharacter::BeginTeleport);
+	PlayerInputComponent->BindAction(TEXT("Action1"), IE_Pressed, this, &AVRCharacter::EnableAction1);
+	PlayerInputComponent->BindAction(TEXT("Action2"), IE_Pressed, this, &AVRCharacter::EnableAction2);
+	PlayerInputComponent->BindAction(TEXT("Action1"), IE_Released, this, &AVRCharacter::DisableAction1);
+	PlayerInputComponent->BindAction(TEXT("Action2"), IE_Released, this, &AVRCharacter::DisableAction2);
 
 }
 
 void AVRCharacter::MoveForward(float throttle)
 {
-	AddMovementInput(throttle * Camera->GetForwardVector());
+	if (bSprint)
+	{
+		AddMovementInput(Camera->GetForwardVector());
+	}
+	else
+	{
+		AddMovementInput(throttle * Camera->GetForwardVector(), 0.5f);
+	}
 }
 
 void AVRCharacter::MoveRight(float throttle)
@@ -113,8 +190,6 @@ void AVRCharacter::BeginTeleport()
 {
 	if (DestinationMarker->IsVisible())
 	{
-		float dot = FVector::DotProduct(GetActorUpVector(), FVector(0, 0, 1));
-		UE_LOG(LogTemp, Warning, TEXT("DOT: %f"), dot);
 		APlayerController* PC = Cast<APlayerController>(GetController());
 		if (PC != nullptr)
 		{
@@ -136,5 +211,29 @@ void AVRCharacter::FinishTeleport()
 	{
 		PC->PlayerCameraManager->StartCameraFade(1, 0, TeleportFadeTime, FLinearColor::Black);
 	}
+}
+
+void AVRCharacter::EnableAction1()
+{
+	bAction1 = true;
+	UE_LOG(LogTemp, Warning, TEXT("Action1"));
+}
+
+void AVRCharacter::EnableAction2()
+{
+	bAction2 = true;
+	UE_LOG(LogTemp, Warning, TEXT("Action2"));
+}
+
+void AVRCharacter::DisableAction1()
+{
+	bAction1 = false;
+	bSprint = false;
+}
+
+void AVRCharacter::DisableAction2()
+{
+	bAction2 = false;
+	bSprint = false;
 }
 
